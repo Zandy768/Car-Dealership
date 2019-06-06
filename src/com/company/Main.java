d889ea3d5f6775da6a503dc5ee2b7e4b76657bbb
Zandy768 <38893749+Zandy768@users.noreply.github.com>
2019-06-06 14:17:30 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,19 +26,27 @@ public class Main
         List<String> parts = new ArrayList<>();
 
         Dealership d1 = new Dealership(0, "Cars4Sale", "123 Cars Road");
+        Dealership d2 = new Dealership(1, “Cars4U”, “555 Five Street”);
         dealer.add(d1.toString());
+        dealer.add(d2.toString());
         System.out.println(dealer);
 
-        SalesPerson s1 = new SalesPerson(0, "Same",0000);
+        SalesPerson s1 = new SalesPerson(0, "Sam",0000);
+        SalesPerson s2 = new SalesPerson(1, “Bob”,1111);
         salesPerson.add(s1.toString());
+        salesPerson.add(s2.toString());
         System.out.println(salesPerson);
 
-        SalesLeads sl1 = new SalesLeads(0,"Same",100);
+        SalesLeads sl1 = new SalesLeads(0,"Sam",100);
+        SalesLeads sl2 = new SalesLeads(1,”Bob”,200);
         salesLead.add(sl1.toString());
+        salesLead.add(sl2.toString());
         System.out.println(salesLead);
 
         Customer c1 = new Customer(0,"Max", 000000000);
+        Customer c2 = new Customer(1,”Chris”,111111111);
         customer.add(c1.toString());
+        customer.add(c2.toString());
         System.out.println(customer);
 
         Inventory i1 = new Inventory(0,"Used Cars",200);
